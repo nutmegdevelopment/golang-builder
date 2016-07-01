f4176ff036e3e7e6bead728b39474035d03a7b5b
James Findley <jamesf@nutmeg.com>
2016-07-01 15:56:54 +0100

    Remove linking from tester

--- tester/build_environment.sh
+++ tester/build_environment.sh
@@ -17,27 +17,5 @@ then
   exit 992
 fi
 
-# Grab just first path listed in GOPATH
-goPath="${GOPATH%%:*}"
-
-# Construct Go package path
-pkgPath="$goPath/src/$pkgName"
-
-# Set-up src directory tree in GOPATH
-mkdir -p "$(dirname "$pkgPath")"
-
-# Link source dir into GOPATH
-ln -sf /src "$pkgPath"
-
-if [ -e "$pkgPath/vendor" ];
-then
-    # Enable vendor experiment
-    export GO15VENDOREXPERIMENT=1
-elif [ -e "$pkgPath/Godeps/_workspace" ];
-then
-  # Add local godeps dir to GOPATH
-  GOPATH=$pkgPath/Godeps/_workspace:$GOPATH
-else
-  # Get all package dependencies
-  go get -t -d -v ./...
-fi
+# Get all package dependencies
+go get -t -d -v ./...
